3f80e8769382b355068ea8b93b025e7b82d46ec6
professorbee <batquartet@gmail.com>
2021-03-12 10:55:34 -0500

    wow this was extremely dumb

--- lib/miku_phoenix_web/router.ex
+++ lib/miku_phoenix_web/router.ex
@@ -10,6 +10,7 @@ defmodule MikuPhoenixWeb.Router do
   end
 
   pipeline :api do
+    plug CORSPlug, origin: "*"
     plug :accepts, ["json"]
   end
 
--- mix.exs
+++ mix.exs
@@ -41,7 +41,8 @@ defmodule MikuPhoenix.MixProject do
       {:telemetry_poller, "~> 0.4"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:cors_plug, "~> 1.5"}
     ]
   end
 
